632b1e65cd237d63140da31c49f81ac5cfea130a
Asaad <asaad1793.z@gmail.com>
2025-05-21 17:14:03 +0300

    file add

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1=10
+number2=5
+sum=number1+number2
+difference=number1-number2
+product=number1*number2
+print("the sum of ",number1,"and",number2,"is",sum)
+print("the difference of ",number1,"and",number2,"is",difference)
+print("the product of ",number1,"and",number2,"is",product)
