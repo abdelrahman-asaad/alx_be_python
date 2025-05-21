46ded702e2e1e7f954f7abbd2197dbaa71123029
Asaad <asaad1793.z@gmail.com>
2025-05-21 19:06:44 +0300

    file add

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,7 @@
+income = int(input("Enter your monthly income: "))
+expanses = int(input("Enter your total monthly expenses: "))
+savings = income - expanses
+interest = 0.05
+savings_after = savings * 12 * 0.05
+print("Your monthly savings are ",savings)
+print("Projected savings after one year,with interest,is",saving_after)
