7e6f32184a36473912844a34a6daec02e6048eaf
Asaad <asaad1793.z@gmail.com>
2025-06-01 21:05:24 +0300

    add

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -9,4 +9,8 @@ match operation :
     case "*" :
         print (("The result is " ), num1 * num2) 
     case "/" :
-        print (("The result is " ), num1 / num2)        
+        if num2 == 0 :
+          print ("Cannot divide by zero.")
+        else :      
+          print (("The result is " ), num1 / num2)   
+                 
