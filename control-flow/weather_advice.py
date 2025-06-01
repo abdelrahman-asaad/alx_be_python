2022382e52baa3a8f722f61ce95d6af68fc4e126
Asaad <asaad1793.z@gmail.com>
2025-06-01 20:50:45 +0300

    add

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,5 +1,9 @@
-wheather = input("What's the weather like today? (sunny/rainy/cold):")
-if wheather == "sunny" :print("Wear a t-shirt and sunglasses.")
-elif wheather == " rainy" :print("Don't forget your umbrella and a raincoat.")    
-elif wheather == " cold" :print("Make sure to wear a warm coat and a scarf.")    
-else :print("Sorry, I don't have recommendations for this weather.")    
+weather = input("What's the weather like today? (sunny/rainy/cold):")
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")    
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")    
+else:
+    print("Sorry, I don't have recommendations for this weather.")    
