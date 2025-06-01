ce1e2063cd623081c4a4c9ef95e53717e13f2cf9
Asaad <asaad1793.z@gmail.com>
2025-06-01 22:22:52 +0300

    add

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,10 +1,21 @@
-# Ask the user to enter the size of the pattern
+# pattern_drawing.py
+
+# Prompt the user for the size of the pattern
 size = int(input("Enter the size of the pattern: "))
 
-# Loop through each row
-for i in range(size):
-    # Print a row of asterisks
-    print('*' * size)
+# Initialize the row counter
+row = 0
 
-        
+# Outer while loop for each row
+while row < size:
+    # Inner for loop for printing asterisks in the current row
+    for col in range(size):
+        print("*", end="")
+    
+    # Move to the next line after printing one row
+    print()
+    
+    # Increment the row counter
+    row += 1
+
 
