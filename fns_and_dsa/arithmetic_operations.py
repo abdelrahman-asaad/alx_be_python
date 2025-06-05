321a10fb50fbd96ed0036e6dc9ba4fad4a6f95ce
Asaad <asaad1793.z@gmail.com>
2025-06-05 22:50:04 +0300

    add

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,20 +1,14 @@
-def perform_operation():
-    print("Arithmetic operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-    if operation == "add":
-        print(num1 + num2)
-    elif operation == "subtract":
-        print(num1 - num2)
-    elif operation == "divide":
-        if num2 != 0:
-            print(num1 / num2)
-        else:
-            print("can't divide by zero ")
-    elif operation == "miltiply":
-        print(num1 * num2)
+def perform_operation(num1: float, num2: float, operation: str):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
     else:
-        input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()  
+        return "Error: Invalid operation"
 
-perform_operation()                             
