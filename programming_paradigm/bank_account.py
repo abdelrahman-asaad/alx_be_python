459d63c3192657e67d72164e7a3502270462cf6d
Asaad <asaad1793.z@gmail.com>
2025-06-15 09:03:11 +0300

    add

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,15 +3,15 @@ class BankAccount:
         self.account_balance = account_balance
     def deposit(self, amount):
         self.account_balance += amount
-        print(f"Deposited: ${amount}")
+        print(f"Deposited: ${amount:.2f}")
     def withdraw(self, amount):
         if self.account_balance < amount:
-            print("Insufficient funds")
+            print("Insufficient funds.")
         else: 
             self.account_balance -= amount   
-            print(f"Withdrew: ${amount}")
+            print(f"Withdrew: ${amount:.2f}")
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
 
 #creating object
 operation = BankAccount(100)
@@ -22,8 +22,6 @@ operation.withdraw(20)
 operation.display_balance()
 operation.withdraw(140)
 operation.display_balance()
-
-
 
 
 
