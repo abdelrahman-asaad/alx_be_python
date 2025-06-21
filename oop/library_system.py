877b775d19c73e4b87a443460fd2e09dfe489d7f
Asaad <asaad1793.z@gmail.com>
2025-06-21 18:05:15 +0300

    added

---
+++ oop/library_system.py
@@ -0,0 +1,57 @@
+#Composition
+class Book:
+    def __init__(self, title:str, author:str):
+        self.title = title
+        self.author = author
+
+    def __repr__(self):  #to represent object instantiated of this class
+        return f"{__class__.__name__}: {self.title} by {self.author}"  
+    
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __repr__(self):   #to represent object instantiated of this class
+        return f"{__class__.__name__}: {self.title} by {self.author}, File Size: {self.file_size}"   
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count:int):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __repr__(self):    #to represent object instantiated of this class
+        return f"{__class__.__name__}: {self.title} by {self.author}, Page Count: {self.page_count}"   
+
+class Library:
+    def __init__(self):
+       self.books = []   #instance level attribute
+    
+
+    def add_book(self, book):
+       self.books.append(book)
+    
+    def list_books(self):
+       for book in self.books:
+           print(book)
+
+
+#creaating object
+classic_book = Book("Pride and Prejudice", "Jane Austen")
+digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
+
+my_library = Library()
+
+ # Add books to the library
+my_library.add_book(classic_book)
+my_library.add_book(digital_novel)
+my_library.add_book(paper_novel)
+
+    # List all books in the library
+my_library.list_books()
+        
+
+    
+    
