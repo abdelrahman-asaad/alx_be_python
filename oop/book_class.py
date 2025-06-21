f3120efa427a962a3d745918eb8a5bd98c5dae6d
Asaad <asaad1793.z@gmail.com>
2025-06-21 16:03:23 +0300

    added

---
+++ oop/book_class.py
@@ -0,0 +1,13 @@
+class Book:
+    def __init__(self, title:str, author:str, publication_year:int):
+        assert publication_year > 0
+        self.title = title
+        self.author = author
+        self.publication_year = publication_year
+    def __del__(self):
+        return f"Deleting {self.title}"
+    def __str__(self):
+        return  f"{self.title} by {self.author}, published in {self.year}"
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+    
