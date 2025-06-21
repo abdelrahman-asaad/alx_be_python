f38035a28c2da15a8c19a64c2e7bf2561419743c
Asaad <asaad1793.z@gmail.com>
2025-06-21 20:25:06 +0300

    added

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,25 @@
+class Calculator:
+    calculation_type = "Arithmetic Operations"  #class level attribute
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def add(x, y):
+        return x + y
+
+    @classmethod
+    def multiply(cls, a, b):
+        cls.calculation_type =  a * b  #class level attribute
+        print(f"Calculation type: {cls.calculation_type}")
+        return cls.calculation_type  # or retutn a * b
+    
+#creating object
+# Using the static method
+sum_result = Calculator.add(10, 5)
+print(f"The sum is: {sum_result}")
+
+# Using the class method
+product_result = Calculator.multiply(10, 5)
+print(f"The product is: {product_result}")
+    
+    
