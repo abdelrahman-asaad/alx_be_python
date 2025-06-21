2b8a1821ab0d2307190d848a1a8a6a1ae0cced36
Asaad <asaad1793.z@gmail.com>
2025-06-21 18:12:45 +0300

    added

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,57 +1,39 @@
-#Composition
+# library_system.py
+
 class Book:
-    def __init__(self, title:str, author:str):
+    def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
 
-    def __str__(self):  #to return the string proven when object instantiated of this class
-        return f"{__class__.__name__}: {self.title} by {self.author}"  
-    
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
 
 class EBook(Book):
-    def __init__(self, title, author, file_size):
+    def __init__(self, title: str, author: str, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def __str__(self):    #to return the string proven when object instantiated of this class
-        return f"{__class__.__name__}: {self.title} by {self.author}, File Size: {self.file_size}"   
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
 
 class PrintBook(Book):
-    def __init__(self, title, author, page_count:int):
+    def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def __str__(self):     #to return the string proven when object instantiated of this class
-        return f"{__class__.__name__}: {self.title} by {self.author}, Page Count: {self.page_count}"   
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
 
 class Library:
     def __init__(self):
-       self.books = []   #instance level attribute
-    
+        self.books = []
 
     def add_book(self, book):
-       self.books.append(book)
-    
+        self.books.append(book)
+
     def list_books(self):
-       for book in self.books:
-           print(book)
-
-
-#creaating object
-classic_book = Book("Pride and Prejudice", "Jane Austen")
-digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
-paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
-
-my_library = Library()
-
- # Add books to the library
-my_library.add_book(classic_book)
-my_library.add_book(digital_novel)
-my_library.add_book(paper_novel)
-
-    # List all books in the library
-my_library.list_books()
-        
-
-    
-    
+        for book in self.books:
+            print(book)
