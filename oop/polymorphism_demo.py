0c8a70b9d5b7114b7a259f7ef5a531fb620a2ede
Asaad <asaad1793.z@gmail.com>
2025-06-21 18:35:33 +0300

    add

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,34 @@
+class Shape:
+    def __init__(self):
+        pass
+    
+    def area(self):
+        raise NotImplementedError
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        super().__init__()    
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+class Circle(Shape):
+    def __init__(self, radius):
+        super().__init__()
+        self.radius = radius
+
+    def area(self):
+        import math
+        return self.radius * self.radius * math.pi    
+    
+#creating object
+shapes = [
+        Rectangle(10, 5),
+        Circle(7)
+    ]
+
+for shape in shapes:
+        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+    
